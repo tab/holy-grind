f351ecd880dba2d64ae47b3d0abc1b75c0163098
tab <tukhfatullin@gmail.com>
2026-02-08 18:14:05 +0200

    fix(vite): Exclude e2e and node_modules from coverage

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     globals: true,
     setupFiles: './src/test/setup.ts',
     css: true,
+    exclude: ['e2e/**', 'node_modules/**'],
     coverage: {
       reporter: ['text', 'cobertura'],
       include: ['src/**/*.{ts,tsx}'],
